a33ea27a3519f21f4623c8c50238ebc6f12ca32f
My Lord <page.c.tyler@gmail.com>
2020-12-08 15:19:57 -0500

    initial commit

---
+++ Main.scala
@@ -0,0 +1,26 @@
+import java.io.PrintWriter
+
+object Solution {
+
+        /*
+         * Complete the simpleArraySum function below.
+         */
+        def simpleArraySum(ar: Array[Int]): Int = {
+        return ar.reduce((x, y) => x + y)
+        }
+
+        def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val arCount = stdin.readLine.trim.toInt
+
+        val ar = stdin.readLine.split(" ").map(_.trim.toInt)
+        val result = simpleArraySum(ar)
+
+        printWriter.println(result)
+
+        printWriter.close()
+        }
+}
